eb029e07fc6fcaf95e98d3a8fb82ab6c103d9829
NeptuneHub <g.colangiuli@gmail.com>
2024-06-01 11:55:46 +0200

    Create restart-longhorn.sh

---
+++ restart-longhorn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#replace the engine name with the correct one of your installation
+kubectl rollout restart daemonset engine-image-ei-b0369a5d -n longhorn-system
+kubectl rollout restart daemonset longhorn-csi-plugin -n longhorn-system
+kubectl rollout restart daemonset longhorn-manager -n longhorn-system
+
+kubectl rollout restart deploy csi-attacher -n longhorn-system
+kubectl rollout restart deploy csi-provisioner -n longhorn-sytem
+kubectl rollout restart deploy csi-resizer -n longhorn-system
+kubectl rollout restart deploy csi-snapshotter -n longhorn-system
+kubectl rollout restart deploy longhorn-driver-deployer -n longhorn-system
+kubectl rollout restart deploy longhorn-ui -n longhorn-system
